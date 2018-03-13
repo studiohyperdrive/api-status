9218d742284607118950b71d0ab90a1b56ba71d2
Sander Houttekier <sander.houttekier@gmail.com>
2018-03-13 17:24:59 +0100

    linting fixes

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// ESLint configuration.
+
+module.exports = {
+	"extends": [
+		"@district01/eslint-config",
+		"@district01/eslint-config/lib/es6.js"
+	],
+
+	// Override rules if needed.
+	// This is NOT recommended. Please report issues you encounter with the @district01/eslint-config
+	// package in the issue queue: https://bitbucket.org/district01/eslint-config/issues.
+	"plugins": [
+		"chai-friendly"
+	],
+	"rules": {
+		// Disable no-unused-expressions
+		"no-unused-expressions": 0,
+		// Enable no-unused-expressions but skip items for chai
+		"chai-friendly/no-unused-expressions": 2
+	},
+};
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-'use strict';
+"use strict";
 
-module.exports = require('./lib/status');
+module.exports = require("./lib/status");
