0b54007723a832a3a677a5ad5aaaa24b426d14bd
Sander Houttekier <sander.houttekier@gmail.com>
2018-03-15 12:24:18 +0100

    update to the latest boilerplate stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,12 @@
 
 module.exports = {
 	"extends": [
-		"@district01/eslint-config",
-		"@district01/eslint-config/lib/es6.js"
+		"@studiohyperdrive/eslint-config",
+		"@studiohyperdrive/eslint-config/lib/es6.js"
 	],
 
 	// Override rules if needed.
-	// This is NOT recommended. Please report issues you encounter with the @district01/eslint-config
+	// This is NOT recommended. Please report issues you encounter with the @studiohyperdrive/eslint-config
 	// package in the issue queue: https://bitbucket.org/district01/eslint-config/issues.
 	"plugins": [
 		"chai-friendly"
